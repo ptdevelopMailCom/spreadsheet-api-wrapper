90e0cd2d54f05fd436990a7754b6b5bbba46ac54
Tony Chan <ptdevelop@mail.com>
2020-01-17 08:50:13 +0000

    Align styles to CommonJS

--- spreadsheet-api/src/google-api/spreadsheet-api/helper.js
+++ spreadsheet-api/src/google-api/spreadsheet-api/helper.js
@@ -1,35 +1,36 @@
-import {google} from 'googleapis';
-import * as moment from 'moment';
+const {google} = require('googleapis');
+const moment = require('moment');
 
-export async function updateStockByStockCode(auth, code) {
-    const sheet = google.sheets('v4');
-    const now = moment().local().format('YYYY,MM,DD');
-    const init = moment.unix(0).format('YYYY,MM,DD');
+module.exports = {
+    updateStockByStockCode: async function(auth, code) {
+        const sheet = google.sheets('v4');
+        const now = moment().local().format('YYYY,MM,DD');
+        const init = moment.unix(0).format('YYYY,MM,DD');
 
-    const request = {
-        spreadsheetId: process.env.SPREADSHEET_ID,
-        auth: auth,
-        range: 'A1',
-        valueInputOption: 'USER_ENTERED',
-        requestBody: {
+        const request = {
+            spreadsheetId: process.env.SPREADSHEET_ID,
+            auth: auth,
             range: 'A1',
-            values: [
-                [`=GOOGLEFINANCE("${code}", "All", DATE(${init}), DATE(${now}), "DAILY")`]
-            ]
+            valueInputOption: 'USER_ENTERED',
+            requestBody: {
+                range: 'A1',
+                values: [
+                    [`=GOOGLEFINANCE("${code}", "All", DATE(${init}), DATE(${now}), "DAILY")`]
+                ]
+            }
         }
-    }
-    return sheet.spreadsheets.values.update(request);
-}
+        return sheet.spreadsheets.values.update(request);
+    },
+    getStockByStockCode: async function(auth, code) {
+        const sheet = google.sheets('v4');
 
-export async function getStockByStockCode(auth, code) {
-    const sheet = google.sheets('v4');
-
-    const request = {
-        spreadsheetId: process.env.SPREADSHEET_ID,
-        auth: auth,
-        ranges: ["A1:F"],
-        valueRenderOption: 'FORMATTED_VALUE',
-        dateTimeRenderOption: 'SERIAL_NUMBER'
+        const request = {
+            spreadsheetId: process.env.SPREADSHEET_ID,
+            auth: auth,
+            ranges: ["A1:F"],
+            valueRenderOption: 'FORMATTED_VALUE',
+            dateTimeRenderOption: 'SERIAL_NUMBER'
+        }
+        return sheet.spreadsheets.values.batchGet(request);
     }
-    return sheet.spreadsheets.values.batchGet(request);
-}
+};
